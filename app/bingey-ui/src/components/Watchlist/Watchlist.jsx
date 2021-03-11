baacd017765ec03308a5de8ba353caec2f6bb82d
sarunas <sarunas.palianskas@ktu.edu>
2021-03-11 11:05:10 +0200

    add header to watchlist component

--- app/bingey-ui/src/components/Watchlist/Watchlist.jsx
+++ app/bingey-ui/src/components/Watchlist/Watchlist.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Grid } from '@material-ui/core';
+import { Paper, Grid, Typography, Button } from '@material-ui/core';
 
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
     margin: theme.spacing(2),
   },
   paper: {
-    aspectRatio: 3 / 4,
+    aspectRatio: 5 / 7,
     padding: theme.spacing(2),
     textAlign: 'center',
     backgroundColor: '#c4c4c4',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  watchlistName: {
+    fontWeight: 'bold',
+    padding: theme.spacing(2, 0),
+  },
+  addTitleHeaderButton: {
+    margin: theme.spacing(2, 0),
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
+    },
   },
   addTitleCard: {
     display: 'flex',
@@ -32,21 +46,35 @@ export const Watchlist = ({ titles }) => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <Typography variant='h4' className={classes.watchlistName}>
+          My Watchlist
+        </Typography>
+        <Button
+          variant='outlined'
+          color='primary'
+          size='large'
+          className={classes.addTitleHeaderButton}
+          endIcon={<AddIcon />}
+        >
+          Add title
+        </Button>
+      </div>
       <Grid container spacing={2}>
         {titles.map((title) => {
           return (
-            <Grid item xs={6} sm={3}>
+            <Grid item xs={6} sm={4} md={3} xl={2}>
               <Paper className={classes.paper}>{title.name}</Paper>
             </Grid>
           );
         })}
-        <Grid item xs={6} sm={3}>
+        <Grid item xs={6} sm={4} md={3} xl={2}>
           <Paper className={clsx(classes.paper, classes.addTitleCard)}>
             <AddCircleOutlineIcon className={classes.addTitleIcon} />
           </Paper>
         </Grid>
       </Grid>
-    </Paper>
+    </div>
   );
 };
