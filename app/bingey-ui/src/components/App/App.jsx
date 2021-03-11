2b858f5df07f06e2e384f62fc9be3e3d91b43c72
sarunas <sarunas.palianskas@ktu.edu>
2021-03-11 11:52:20 +0200

    integrate watchlist into main page

--- app/bingey-ui/src/components/App/App.jsx
+++ app/bingey-ui/src/components/App/App.jsx
@@ -1,9 +1,39 @@
 import { NavigationAppBar } from 'components/Navigation/Appbar/Appbar';
+import { Watchlist } from 'components/Watchlist/Watchlist';
 
 export const App = () => {
+  const data = [
+    {
+      name: '1 title name',
+    },
+    {
+      name: '2 title name',
+    },
+    {
+      name: '3 title name',
+    },
+    {
+      name: '4 title name',
+    },
+    {
+      name: '5 title name',
+    },
+    {
+      name: '6 title name',
+    },
+    {
+      name: '7 title name',
+    },
+  ];
+
   return (
-    <header>
-      <NavigationAppBar title='Bingey' />
-    </header>
+    <>
+      <header>
+        <NavigationAppBar title='Bingey' />
+      </header>
+      <body>
+        <Watchlist titles={data} />
+      </body>
+    </>
   );
 };
