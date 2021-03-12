ac1f9d43331e6d6d467f0c111f7f55d4f89914a1
sarunas <sarunas.palianskas@ktu.edu>
2021-03-12 23:37:23 +0200

    BING-19: port drawer styling to scss and add navigation  to main app component

--- app/bingey-ui/src/components/App/App.jsx
+++ app/bingey-ui/src/components/App/App.jsx
@@ -1,35 +1,39 @@
-import { NavigationAppBar } from 'components/Navigation/Appbar/Appbar';
+import { Navigation } from 'components/Navigation/Navigation';
 import { Watchlist } from 'components/Watchlist/Watchlist';
 
-export const App = () => {
-  const data = [
-    {
-      name: '1 title name',
-    },
-    {
-      name: '2 title name',
-    },
-    {
-      name: '3 title name',
-    },
-    {
-      name: '4 title name',
-    },
-    {
-      name: '5 title name',
-    },
-    {
-      name: '6 title name',
-    },
-    {
-      name: '7 title name',
-    },
-  ];
+import appStyle from './appStyle.scss';
 
+const data = [
+  {
+    name: '1 title name',
+  },
+  {
+    name: '2 title name',
+  },
+  {
+    name: '3 title name',
+  },
+  {
+    name: '4 title name',
+  },
+  {
+    name: '5 title name',
+  },
+  {
+    name: '6 title name',
+  },
+  {
+    name: '7 title name',
+  },
+];
+
+export const App = () => {
   return (
     <>
-      <NavigationAppBar title='Bingey' />
-      <Watchlist titles={data} />
+      <Navigation title='Bingey' drawerWidth={240} />
+      <main className='content'>
+        <Watchlist titles={data} />
+      </main>
     </>
   );
 };
--- app/bingey-ui/src/components/Navigation/Drawer/Drawer.jsx
+++ app/bingey-ui/src/components/Navigation/Drawer/Drawer.jsx
@@ -11,6 +11,8 @@ import MovieIcon from '@material-ui/icons/Movie';
 import StarsIcon from '@material-ui/icons/Stars';
 import SettingsIcon from '@material-ui/icons/Settings';
 
+import drawerStyles from './drawerStyle.scss';
+
 const defaultProps = {
   isOpen: false,
   width: 240,
@@ -20,13 +22,6 @@ const defaultProps = {
 };
 
 const useStyles = makeStyles((theme) => ({
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
   drawerOpen: ({ drawerWidth }) => ({
     width: drawerWidth,
     transition: theme.transitions.create('width', {
@@ -37,86 +32,57 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
     },
   }),
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    // width: theme.spacing(5) + 1,
-    width: theme.spacing(9) + 1,
-    // [theme.breakpoints.up('sm')]: {
-    //   width: theme.spacing(9) + 1,
-    // },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    padding: theme.spacing(0, 1),
-  },
 }));
 
 export const NavigationDrawer = ({ isOpen, width, onClose }) => {
-  console.log(width);
-
   const classes = useStyles({ drawerWidth: width });
   const theme = useTheme();
 
   return (
-    <>
-      <Drawer
-        variant='permanent'
-        className={clsx(classes.drawer, {
+    <Drawer
+      variant='permanent'
+      className={clsx('drawer', {
+        [classes.drawerOpen]: isOpen,
+        ['drawerClose']: !isOpen,
+      })}
+      classes={{
+        paper: clsx({
           [classes.drawerOpen]: isOpen,
-          [classes.drawerClose]: !isOpen,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: isOpen,
-            [classes.drawerClose]: !isOpen,
-          }),
-        }}
-      >
-        {/* <div className={clsx({[classes.hide]: !isOpen,})}> */}
-        <div className={classes.content}>
-          <div className={classes.toolbar}>
-            <IconButton onClick={onClose}>
-              {theme.direction === 'ltr' ? (
-                <ChevronLeftIcon />
-              ) : (
-                <ChevronRightIcon />
-              )}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            <ListItem button key={'watchlist'}>
-              <ListItemIcon>
-                <MovieIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Watchlist'} />
-            </ListItem>
-            <ListItem button key={'recommended'}>
-              <ListItemIcon>
-                <StarsIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Recommended'} />
-            </ListItem>
-            <ListItem button key={'settings'}>
-              <ListItemIcon>
-                <SettingsIcon />
-              </ListItemIcon>
-              <ListItemText primary={'Settings'} />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
-    </>
+          ['drawerClose']: !isOpen,
+        }),
+      }}
+    >
+      <div className='toolbar'>
+        <IconButton onClick={onClose}>
+          {theme.direction === 'ltr' ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </div>
+      <Divider />
+      <List className='drawerContent'>
+        <ListItem button key={'watchlist'}>
+          <ListItemIcon>
+            <MovieIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Watchlist'} />
+        </ListItem>
+        <ListItem button key={'recommended'}>
+          <ListItemIcon>
+            <StarsIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Recommended'} />
+        </ListItem>
+        <ListItem button key={'settings'}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Settings'} />
+        </ListItem>
+      </List>
+    </Drawer>
   );
 };
 
