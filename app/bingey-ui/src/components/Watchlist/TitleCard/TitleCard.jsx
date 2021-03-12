904e096682c7356882a6e9d90bf5cf45822fd80f
vytautel <vytaute.lipeikaite@gmail.com>
2021-03-12 17:54:20 +0200

    design title dialog

--- app/bingey-ui/src/components/Watchlist/TitleCard/TitleCard.jsx
+++ app/bingey-ui/src/components/Watchlist/TitleCard/TitleCard.jsx
@@ -5,8 +5,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-import './cardStyle.scss';
-//import TitleDialog from './TitleDialog';
+import 'components/Watchlist/TitleCard/cardStyle.scss';
+import TitleDialog from './TitleDialog';
 
 export default function TitleCard({ titles }) {
   const [open, setOpen] = useState(false);
@@ -20,26 +20,28 @@ export default function TitleCard({ titles }) {
   };
 
   return (
-    <Card>
-      <CardActionArea onClick={handleOpenDialog}>
-        <CardMedia
-          className='media'
-          style={{ paddingTop: '60%' }}
-          image={titles.image}
-          title='Movie poster'
-        />
-        <CardContent>
-          <Typography className='center' gutterBottom variant='body1'>
-            {titles.name}
-          </Typography>
-          <Typography className='inline' variant='body1' color='text'>
-            {titles.upcomingItem}:{' '}
-            <span className='right'> {titles.releaseDate}</span>
-          </Typography>
+    <div>
+      <Card>
+        <CardActionArea onClick={handleOpenDialog}>
+          <CardMedia
+            className='media'
+            style={{ paddingTop: '60%' }}
+            image={titles.image}
+            title='Movie poster'
+          />
+          <CardContent>
+            <Typography className='center' gutterBottom variant='body1'>
+              {titles.name}
+            </Typography>
+            <Typography className='inline' variant='body1' color='text'>
+              {titles.upcomingItem}: {titles.releaseDate}
+            </Typography>
 
-          <Typography variant='body1'>Genre: {titles.genre}</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            <Typography variant='body1'>Genre: {titles.genre}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      <TitleDialog open={open} onClose={handleCloseDialog} titles={titles} />
+    </div>
   );
 }
