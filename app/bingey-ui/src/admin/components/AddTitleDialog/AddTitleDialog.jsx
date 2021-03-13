f8441c3edcd677acfaca650a02f58dc66373f1ca
sarunas <sarunas.palianskas@ktu.edu>
2021-03-13 12:00:00 +0200

    implement admin-add-title form validation

--- app/bingey-ui/src/admin/components/AddTitleDialog/AddTitleDialog.jsx
+++ app/bingey-ui/src/admin/components/AddTitleDialog/AddTitleDialog.jsx
@@ -13,16 +13,17 @@ import {
   FormControlLabel,
   Checkbox,
   Typography,
+  Chip,
 } from '@material-ui/core';
-
-import { Autocomplete } from '@material-ui/lab';
 
 import addTitleDialogStyle from './addTitleDialogStyle.scss';
 
 export const AddTitleDialog = ({ isOpen, onClose }) => {
-  const { register, errors, handleSubmit } = useForm();
+  const { register, errors, handleSubmit, getValues } = useForm();
 
   const [isMovie, setIsMovie] = useState(false);
+
+  // const [directors, setDirectors] = useState([{ name: 'test dir' }]);
 
   const handleClose = () => {
     onClose(false);
@@ -90,15 +91,10 @@ export const AddTitleDialog = ({ isOpen, onClose }) => {
           <TextField
             type='date'
             name='releaseDate'
-            required
             fullWidth
             margin='normal'
             inputRef={register({
               required: 'Release date is  is required',
-              pattern: {
-                value: '[d, 2]/[d, 2]/[d, 4]/',
-                message: 'Invalid date',
-              },
             })}
             error={errors.releaseDate}
             helperText={
@@ -109,21 +105,61 @@ export const AddTitleDialog = ({ isOpen, onClose }) => {
                 : 'First episode release date'
             }
           />
+          <TextField
+            label='Directors'
+            required
+            name='directors'
+            fullWidth
+            inputRef={register({
+              required: 'Specify at least on director',
+            })}
+            error={errors.directors}
+            helperText={
+              errors.directors
+                ? errors.directors.message
+                : 'Comma separated values'
+            }
+          />
+          {/* could't figure out how to validate autocomplete. Using roc Controller failed 
           <Autocomplete
             multiple
             freeSolo
-            id='tags-standard'
-            getOptionLabel={(option) => option.title}
+            options={[]}
+            renderTags={(value, getTagProps) =>
+              value.map((option, index) => (
+                <Chip label={option} {...getTagProps({ index })} />
+              ))
+            }
             renderInput={(params) => (
               <TextField
+                error={errors.directors}
+                helperText={errors.directors ? errors.directors.message : ''}
+                variant='filled'
+                label='Directors'
                 {...params}
-                variant='standard'
-                label='Multiple values'
-                placeholder='Favorites'
               />
             )}
+            name='directors'
+            inputRef={register({
+              validate: {
+                required: (value) => value || 'Specify at least on director',
+              },
+            })}
+          /> */}
+
+          <TextField
+            label='Genres'
+            required
+            name='genres'
+            fullWidth
+            inputRef={register({
+              required: 'Specify at least on genre',
+            })}
+            error={errors.genres}
+            helperText={
+              errors.genres ? errors.genres.message : 'Comma separated values'
+            }
           />
-          <TextField label='Genres' required fullWidth />
           <FormGroup
             row
             className={clsx('inputGroup', {
@@ -132,31 +168,103 @@ export const AddTitleDialog = ({ isOpen, onClose }) => {
           >
             <TextField
               label='Season count'
+              name='seasonCount'
               type='number'
+              inputProps={{ min: '1', step: '1' }}
               required
               className='smallInput'
+              inputRef={register({
+                required: 'Season count is required',
+                min: {
+                  value: 1,
+                  message: 'Count must be greater than zero',
+                },
+                validate: {
+                  integer: (value) =>
+                    parseInt(value).toString() === value ||
+                    'Season count must be an integer',
+                },
+              })}
+              error={errors.seasonCount}
+              helperText={errors.seasonCount ? errors.seasonCount.message : ''}
             />
             <TextField
               label='Episodes per season'
+              name='seasonEpisodes'
               required
               helperText='Comma separated values for each season'
               className='smallInput'
+              inputRef={register({
+                required: 'Season episode count is required',
+                validate: {
+                  justEnoughEpisodes: (value) =>
+                    value.split(',').length == getValues('seasonCount') ||
+                    'Specify length of each season',
+                },
+              })}
+              error={errors.seasonEpisodes}
+              helperText={
+                errors.seasonEpisodes ? errors.seasonEpisodes.message : ''
+              }
             />
             <TextField
               helperText='Latest episode release date'
+              name='latestReleaseDate'
               type='date'
               required
               className='smallInput'
+              inputRef={register({
+                required: 'Latest release date is  is required',
+              })}
+              error={errors.latestReleaseDate}
+              helperText={
+                errors.latestReleaseDate
+                  ? errors.latestReleaseDate.message
+                  : 'Latest episode release date'
+              }
+              margin='normal'
             />
             <TextField
               helperText='Upcoming episode release date'
+              name='nextReleaseDate'
               type='date'
               required
               className='smallInput'
+              inputRef={register({
+                required: 'Next release date is  is required',
+              })}
+              error={errors.nextReleaseDate}
+              helperText={
+                errors.nextReleaseDate
+                  ? errors.nextReleaseDate.message
+                  : 'Next episode release date'
+              }
+              margin='normal'
             />
           </FormGroup>
-          <TextField label='Plot' required fullWidth />
-          <TextField label='Poster image url' required fullWidth />
+
+          <TextField
+            label='Plot'
+            name='plot'
+            required
+            fullWidth
+            inputRef={register({
+              required: 'Plot is required',
+            })}
+            error={errors.plot}
+            helperText={errors.plot ? errors.plot.message : ''}
+          />
+          <TextField
+            label='Poster image url'
+            name='posterUrl'
+            required
+            fullWidth
+            inputRef={register({
+              required: 'Poster image url is required',
+            })}
+            error={errors.posterUrl}
+            helperText={errors.posterUrl ? errors.posterUrl.message : ''}
+          />
           <TextField
             label='Similars'
             fullWidth
