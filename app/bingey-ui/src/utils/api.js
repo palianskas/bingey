1f1e6d6bf38eef88e38aa3b50627f2bd2025e8be
sarunas <sarunas.palianskas@ktu.edu>
2021-03-23 16:32:56 +0200

    BING-51: move api abstraction exports to end of file

--- app/bingey-ui/src/utils/api.js
+++ app/bingey-ui/src/utils/api.js
@@ -6,7 +6,7 @@ const formatStringForSearch = (value) => {
   return value.toLowerCase().replace(' ', '+');
 };
 
-export const search = async (queryString) => {
+const search = async (queryString) => {
   queryString = formatStringForSearch(queryString);
   try {
     return (await axios.get(`${BASE_URL}/search?q=${queryString}`)).data;
@@ -15,10 +15,12 @@ export const search = async (queryString) => {
   }
 };
 
-export const createTitle = async (title) => {
+const createTitle = async (title) => {
   try {
     return (await axios.post(`${BASE_URL}/titles`, title)).data;
   } catch (error) {
     // handle error
   }
 };
+
+export { search, createTitle };
