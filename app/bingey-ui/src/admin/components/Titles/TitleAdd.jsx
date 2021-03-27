679d642b91a2e0ba14d09d80dda7fa73c486595b
sarunas <sarunas.palianskas@ktu.edu>
2021-03-27 19:59:39 +0200

    BING-34: add 'addTitle' dialog handling to admin panel's 'add title' screen

--- app/bingey-ui/src/admin/components/Titles/TitleAdd.jsx
+++ app/bingey-ui/src/admin/components/Titles/TitleAdd.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+
+import { AddTitleDialog } from 'admin/components/AddTitleDialog/AddTitleDialog';
 
 const TitleAdd = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
       <p>This is where you can add titles</p>
+      <Button variant='contained' onClick={handleOpen}>
+        Add new title
+      </Button>
+      <AddTitleDialog isOpen={open} handleClose={handleClose} />
     </div>
   );
 };
