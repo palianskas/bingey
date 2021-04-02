62736078581209513ab31ae8f571a7ef5ac72905
sarunas <sarunas.palianskas@ktu.edu>
2021-04-02 18:17:01 +0300

    BING-53: refactor user model to accomodate watchlist model's changes

--- app/bingey-api/models/user.js
+++ app/bingey-api/models/user.js
@@ -1,41 +1,47 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     registrationDate: {
-        type: Date,
-        required: true,
+      type: Date,
+      required: true,
     },
     watchlists: [
-        {
-            watchlistId: {
-                type: Schema.Types.ObjectId,
-                ref: 'Watchlist',
-            },
-        }
+      {
+        id: {
+          type: Schema.Types.ObjectId,
+          ref: 'Watchlist',
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+      },
     ],
-    
-	recommended: [
-		{
-            titleId: {
-                type: Schema.Types.ObjectId,
-                ref: 'Title',
-            },
-            name: String,
-			imgUrl: {
-                type: String,
-            },
-		}
-	]
-},
-  { timestamps: true, }
+
+    recommended: [
+      {
+        titleId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Title',
+        },
+        name: String,
+        imgUrl: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 const User = model('User', userSchema);
