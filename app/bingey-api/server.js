8e12c2d3af8b888ac7118acdafb535a700dc1ec4
sarunas <sarunas.palianskas@ktu.edu>
2021-04-20 10:53:38 +0300

    BING-58: clean up

--- app/bingey-api/server.js
+++ app/bingey-api/server.js
@@ -2,18 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config({ path: '../.env' });
-// require('dotenv').config();
 
 const searchRoutes = require('$/routes/search');
 const titleRoutes = require('$/routes/title');
 const watchlistRoutes = require('$/routes/watchlist');
 
 const { MONGODB_HOSTNAME, MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
-// const uri = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOSTNAME}/bingey?authSource=admin`;
-
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-//   err ? console.info(err) : console.info('connected to database');
-// });
 
 mongoose.connect(
   `mongodb://${MONGODB_HOSTNAME}:27017`,
