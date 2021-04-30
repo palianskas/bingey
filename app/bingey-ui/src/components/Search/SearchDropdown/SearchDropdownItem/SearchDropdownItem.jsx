5bf4573140eb19582cdf555e78afe6b58dc92c5f
sarunas <sarunas.palianskas@ktu.edu>
2021-04-30 19:46:53 +0300

    BING-61: create 'open title-dialog on dropdown item click' functionality

--- app/bingey-ui/src/components/Search/SearchDropdown/SearchDropdownItem/SearchDropdownItem.jsx
+++ app/bingey-ui/src/components/Search/SearchDropdown/SearchDropdownItem/SearchDropdownItem.jsx
@@ -2,30 +2,30 @@ import { Divider, ListItem, ListItemText, Typography } from '@material-ui/core';
 
 import './searchDropdownItemStyle.scss';
 
-export const SearchDropdownItem = ({ item }) => {
+export const SearchDropdownItem = ({ item, onClick }) => {
   return (
-    <div>
-      <ListItem className='item' alignItems='flex-start'>
+    <>
+      <ListItem className='item' alignItems='flex-start' onClick={onClick}>
         <ListItemText
           className='text'
           primary={
             <Typography variant='h5' className='primary'>
-              {item.name}
+              {item?.name}
             </Typography>
           }
           secondary={
             <Typography variant='h5' className='secondary'>
-              {item.releaseDate}
+              {item?.releaseDate}
             </Typography>
           }
         />
         <img
-          src={item.imageUrl}
+          src={item?.imageUrl}
           className='image'
-          alt={`${item.name} poster`}
+          alt={`${item?.name} poster`}
         ></img>
       </ListItem>
       <Divider />
-    </div>
+    </>
   );
 };
