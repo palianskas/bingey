8904635ee29c628c66d2cfc8f58c04b864161ac1
sarunas <sarunas.palianskas@ktu.edu>
2021-05-02 10:46:18 +0300

    BING-62: add add-title-to-watchlist controller

---
+++ app/bingey-ui/src/components/Watchlist/TitleDialog/AddTitleToWatchlist/AddTitleToWatchlistController.jsx
@@ -0,0 +1,91 @@
+import { useState, useRef, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Snackbar } from '@material-ui/core';
+
+import { AddTitleToWatchlistButton } from 'components/Watchlist/TitleDialog/AddTitleToWatchlist/AddTitleToWatchlistButton';
+import { WatchlistsDropdown } from 'components/Watchlist/TitleDialog/AddTitleToWatchlist/WatchlistsDropdown';
+
+import api from 'utils/api';
+
+import './addTitleToWatchlistStyle.scss';
+
+const formatTitle = (title) => {
+  return {
+    name: title.name,
+    releaseDate: new Date(title.releaseDate).toLocaleDateString('lt-LT'),
+    imageUrl: title.imageUrl,
+    upcomingEpisode: title.upcomingEpisode,
+  };
+};
+
+export const AddTitleToWatchlistController = ({ title }) => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [watchlists, setWatchlists] = useState([]);
+
+  const buttonRef = useRef('test');
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const loadWatchlist = async () => {
+      setWatchlists(await api.getWatchlists());
+    };
+
+    loadWatchlist();
+  }, []);
+
+  const handleOpenDialog = () => {
+    setIsOpenMenu(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsOpenMenu(false);
+  };
+
+  const handleOpenAlert = () => {
+    setIsOpenAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setIsOpenAlert(false);
+  };
+
+  const handleAddToWatchlist = async (watchlist) => {
+    const result = await api.addTitleToWatchlist(
+      watchlist._id,
+      formatTitle(title)
+    );
+
+    if (result.name == watchlist.name) {
+      handleOpenAlert();
+    }
+
+    handleCloseDialog();
+  };
+
+  return (
+    <>
+      <AddTitleToWatchlistButton ref={buttonRef} onClick={handleOpenDialog} />
+      <WatchlistsDropdown
+        watchlists={watchlists}
+        selectedWatchlistId={id}
+        open={isOpenMenu}
+        onClose={handleCloseDialog}
+        onItemClick={handleAddToWatchlist}
+        anchorEl={buttonRef.current}
+      />
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={isOpenAlert}
+        autoHideDuration={2000}
+        onClose={handleCloseAlert}
+        message='Added title to watchlist!'
+      />
+    </>
+  );
+};
