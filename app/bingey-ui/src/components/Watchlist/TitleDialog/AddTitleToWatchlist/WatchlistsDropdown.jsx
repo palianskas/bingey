9961322d27d8d848c45fcfa1e1fdd421d02f0c1e
sarunas <sarunas.palianskas@ktu.edu>
2021-05-02 10:44:08 +0300

    BING-62: design watchlist dropdown

---
+++ app/bingey-ui/src/components/Watchlist/TitleDialog/AddTitleToWatchlist/WatchlistsDropdown.jsx
@@ -0,0 +1,49 @@
+import {
+  Paper,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+  Popover,
+} from '@material-ui/core';
+
+export const WatchlistsDropdown = ({
+  watchlists,
+  selectedWatchlistId,
+  open,
+  anchorEl,
+  onClose,
+  onItemClick,
+}) => {
+  return (
+    <Popover
+      open={open}
+      anchorEl={anchorEl}
+      anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+      disablePortal
+    >
+      <Paper>
+        <ClickAwayListener onClickAway={onClose}>
+          <MenuList autoFocusItem variant='selectedMenu'>
+            {watchlists.map((watchlist) => (
+              <MenuItem
+                onClick={() => onItemClick(watchlist)}
+                className='item'
+                key={watchlist?._id}
+              >
+                <span
+                  className={
+                    watchlist?._id == selectedWatchlistId
+                      ? 'item-text selected'
+                      : 'item-text'
+                  }
+                >
+                  {watchlist?.name}
+                </span>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </ClickAwayListener>
+      </Paper>
+    </Popover>
+  );
+};
