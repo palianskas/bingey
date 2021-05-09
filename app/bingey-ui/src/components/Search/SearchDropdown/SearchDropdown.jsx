866897286ee9e4dcff5f2dbde3c17f1e024a3fae
sarunas <sarunas.palianskas@ktu.edu>
2021-05-09 18:48:39 +0300

    BING-37: add loading spinner to search dropdown

--- app/bingey-ui/src/components/Search/SearchDropdown/SearchDropdown.jsx
+++ app/bingey-ui/src/components/Search/SearchDropdown/SearchDropdown.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
-import { Collapse, Paper } from '@material-ui/core';
+import { CircularProgress, Collapse, Paper } from '@material-ui/core';
 
 import { SearchDropdownItem } from 'components/Search/SearchDropdown/SearchDropdownItem/SearchDropdownItem';
 import { TitleDialog } from 'components/Watchlist/TitleDialog/TitleDialog';
 
 import './searchDropdownStyle.scss';
 
-export const SearchDropdown = ({ open, titles, onClose }) => {
+export const SearchDropdown = ({
+  open,
+  titles,
+  isQueryInProgress,
+  onClose,
+}) => {
   const [selectedItem, setSelectedItem] = useState(null);
   const [isOpenDialog, setIsOpenDialog] = useState(false);
 
   const handleItemClick = (selectedItem) => {
-    // TODO need to get full selectedItem title from api here
     setSelectedItem(selectedItem);
     setIsOpenDialog(true);
     onClose();
@@ -19,19 +23,26 @@ export const SearchDropdown = ({ open, titles, onClose }) => {
 
   const handleCloseDialog = () => {
     setIsOpenDialog(false);
+    onClose();
   };
 
   return (
     <>
       <Paper className={'dropdown'}>
         <Collapse in={open} timeout={250}>
-          {titles?.map((item) => (
-            <SearchDropdownItem
-              item={item}
-              key={item?._id}
-              onClick={() => handleItemClick(item)}
-            />
-          ))}
+          {isQueryInProgress ? (
+            <div className='dropdown-spinner'>
+              <CircularProgress />
+            </div>
+          ) : (
+            titles?.map((item) => (
+              <SearchDropdownItem
+                item={item}
+                key={item?._id}
+                onClick={() => handleItemClick(item)}
+              />
+            ))
+          )}
         </Collapse>
       </Paper>
       <TitleDialog
