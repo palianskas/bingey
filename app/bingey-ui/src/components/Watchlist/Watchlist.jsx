66b8bacfee09c9f5be2f6d82c38a04d72cdfde5c
sarunas <sarunas.palianskas@ktu.edu>
2021-05-13 12:54:07 +0300

    BING-64: remove unused imports

--- app/bingey-ui/src/components/Watchlist/Watchlist.jsx
+++ app/bingey-ui/src/components/Watchlist/Watchlist.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Grid, Typography, Button } from '@material-ui/core';
 
